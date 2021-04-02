ca3158b856c3a0e4ff63e4f7f1ade2bfb9985d69
Cedrei <einar.ceder@gmail.com>
2021-04-02 12:34:20 +0200

    Create webpage

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const http = require("http")
+
 // List of all bot names, as they appear in /scripts
 let botNames = ["ash"]
 let botInstances = []
@@ -8,3 +10,13 @@ for (let i of botNames) {
 	let botClass = require(`./scripts/${i}-main.js`)
 	botInstances.push(new botClass())
 }
+
+
+// Heroku requires us to host an actual page, or it will kill the app after 60 seconds
+http.createServer( function (request, response) {  
+   	response.writeHead(200, {'Content-Type': 'text/html'});	
+         
+   	response.write('Hi! This server runs a discord bot, not a web page. You can invite it <a href="https://discord.com/oauth2/authorize?client_id=693805625804587079&scope=bot&permissions=8">here</a>!');		
+
+    response.end(); 
+}).listen(process.env.PORT || 5000);
