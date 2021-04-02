049d40cc988c44ed36fd495ffef748aea39735e8
Cedrei <einar.ceder@gmail.com>
2021-04-02 16:10:07 +0200

    Update ready.js

--- scripts/ash-events/ready.js
+++ scripts/ash-events/ready.js
@@ -1,5 +1,5 @@
 module.exports = bot => {
 	// First update the leaderboards once, then do it repeatedly once per minute
 	bot.updateLeaderboards()
-	setInterval(bot.updateLeaderboards, 60*1000)
+	setInterval(() => bot.updateLeaderboards(), 60*1000)
 }
