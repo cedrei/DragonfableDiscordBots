69978367dc6f2e5f4db6424a5a7761e86cb16235
Cedrei <einar.ceder@gmail.com>
2021-09-19 20:39:24 +0200

    Update guildMemberAdd.js

--- scripts/hacke-events/guildMemberAdd.js
+++ scripts/hacke-events/guildMemberAdd.js
@@ -2,4 +2,5 @@ module.exports = (bot, member) => {
   bot.client.channels.fetch('883009554286268509')
     .then(channel => channel.send("Welcome, <@" + member.id + ">! https://youtu.be/A_IDGrKZ0Rs"))
     .catch(console.error);
+    member.roles.add("889215376620154941")
 }
